97b1f8bb6e3cb800e7d2d65cbc13a66dc6fb9856
Phan Anh Minh <phananhminh24@gmail.com>
2025-03-11 09:56:07 +0700

    first commit

---
+++ src/main/java/com/wbsrisktaskerx/wbsrisktaskerx/WbsrisktaskerxApplication.java
@@ -0,0 +1,13 @@
+package com.wbsrisktaskerx.wbsrisktaskerx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WbsrisktaskerxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WbsrisktaskerxApplication.class, args);
+	}
+
+}
