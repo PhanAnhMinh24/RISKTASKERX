7b91e8d71bd7f59bf178062a67b26b37e63e0d42
xuanngoczxc <xuanngoczxc@gmail.com>
2025-03-13 16:34:29 +0700

    Setup send email

---
+++ src/main/java/com/wbsrisktaskerx/wbsrisktaskerx/common/contants/EmailContant.java
@@ -0,0 +1,9 @@
+package com.wbsrisktaskerx.wbsrisktaskerx.common.contants;
+
+public class EmailContant {
+
+    public static final String PLACEHOLDER_CONTENT = "{{content}}";
+    public static final String PLACEHOLDER_SUBJECT = "{{subject}}";
+    public static final String PLACEHOLDER_TEMPLATES_EMAIL = "templates/email-template.html";
+
+}
---
+++ src/main/java/com/wbsrisktaskerx/wbsrisktaskerx/service/EmailService.java
@@ -0,0 +1,44 @@
+package com.wbsrisktaskerx.wbsrisktaskerx.service;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Map;
+
+@Service
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    @Value("${spring.mail.username}")
+    private String fromEmail;
+
+    public void sendEmail(String to, String subject, String templatePath, Map<String, String> placeholders)
+            throws MessagingException, IOException {
+        MimeMessage message = javaMailSender.createMimeMessage();
+        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(message, true, "UTF-8");
+
+        mimeMessageHelper.setTo(to);
+        mimeMessageHelper.setSubject(subject);
+        mimeMessageHelper.setFrom(fromEmail);
+
+        String htmlTemplate = new String(Files.readAllBytes(Paths.get(new ClassPathResource(templatePath).getURI())));
+
+        for (Map.Entry<String, String> entry : placeholders.entrySet()) {
+            htmlTemplate = htmlTemplate.replace(entry.getKey(), entry.getValue());
+        }
+
+        mimeMessageHelper.setText(htmlTemplate, true);
+        javaMailSender.send(message);
+    }
+}
