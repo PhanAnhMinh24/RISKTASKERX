ad92aaa18c9a832230bd73417a7daed6c4ee781b
FPTSHOP <goodluckforyou2025@gmail.com>
2025-03-26 10:13:37 +0700

    Customer-details

--- src/main/java/com/wbsrisktaskerx/wbsrisktaskerx/config/SecurityConfiguration.java
+++ src/main/java/com/wbsrisktaskerx/wbsrisktaskerx/config/SecurityConfiguration.java
@@ -1,6 +1,5 @@
 package com.wbsrisktaskerx.wbsrisktaskerx.config;
 
-
 import com.wbsrisktaskerx.wbsrisktaskerx.service.admin.AdminDetailsServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
