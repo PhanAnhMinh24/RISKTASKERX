d57b607795f89fc548763f7c6d3852abc261f707
FPTSHOP <goodluckforyou2025@gmail.com>
2025-03-26 13:38:21 +0700

    Customer-details

--- src/main/java/com/wbsrisktaskerx/wbsrisktaskerx/common/constants/PaymentMethodConstants.java
+++
@@ -1,7 +0,0 @@
-package com.wbsrisktaskerx.wbsrisktaskerx.common.constants;
-
-public class PaymentMethodConstants {
-    public static final String CREDIT_CARD = "credit_card";
-    public static final String BANK_TRANSFER = "bank_transfer";
-    public static final String CASH = "cash";
-}
