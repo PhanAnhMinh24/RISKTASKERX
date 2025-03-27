f0222dca7ea319e5b20880a1c4245a62fad8ee8e
Phan Anh Minh <phananhminh24@gmail.com>
2025-03-27 17:32:57 +0700

    Hide information customer

--- src/main/java/com/wbsrisktaskerx/wbsrisktaskerx/serializer/MaskingAddressSerializer.java
+++ src/main/java/com/wbsrisktaskerx/wbsrisktaskerx/serializer/MaskingAddressSerializer.java
@@ -2,6 +2,6 @@ package com.wbsrisktaskerx.wbsrisktaskerx.serializer;
 
 public class MaskingAddressSerializer extends MaskingSerializer {
     public MaskingAddressSerializer() {
-        super(4); // Hiển thị 4 ký tự đầu tiên
+        super(4);
     }
 }
--- src/main/java/com/wbsrisktaskerx/wbsrisktaskerx/serializer/MaskingFullNameSerializer.java
+++ src/main/java/com/wbsrisktaskerx/wbsrisktaskerx/serializer/MaskingFullNameSerializer.java
@@ -2,6 +2,6 @@ package com.wbsrisktaskerx.wbsrisktaskerx.serializer;
 
 public class MaskingFullNameSerializer extends MaskingSerializer {
     public MaskingFullNameSerializer() {
-        super(3); // Hiển thị 3 ký tự đầu tiên
+        super(3);
     }
 }
