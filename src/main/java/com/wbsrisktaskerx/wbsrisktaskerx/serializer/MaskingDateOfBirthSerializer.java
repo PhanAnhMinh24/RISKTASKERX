005eb28fad1127e6bf32d39171e89717e44d91a7
Phan Anh Minh <phananhminh24@gmail.com>
2025-03-27 17:32:57 +0700

    Hide information customer

--- src/main/java/com/wbsrisktaskerx/wbsrisktaskerx/serializer/MaskingDateOfBirthSerializer.java
+++ src/main/java/com/wbsrisktaskerx/wbsrisktaskerx/serializer/MaskingDateOfBirthSerializer.java
@@ -2,6 +2,6 @@ package com.wbsrisktaskerx.wbsrisktaskerx.serializer;
 
 public class MaskingDateOfBirthSerializer extends MaskingSerializer {
     public MaskingDateOfBirthSerializer() {
-        super(0); // Ẩn hoàn toàn, không hiển thị ký tự nào
+        super(0);
     }
 }
