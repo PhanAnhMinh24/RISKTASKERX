53f85d8de54d5f11322a8fce2ab7857f55bd831c
xuanngoczxc <xuanngoczxc@gmail.com>
2025-04-01 11:35:56 +0700

    Export History Customer

--- src/main/java/com/wbsrisktaskerx/wbsrisktaskerx/service/export/ExportDetails.java
+++ src/main/java/com/wbsrisktaskerx/wbsrisktaskerx/service/export/ExportDetails.java
@@ -1,11 +1,15 @@
 package com.wbsrisktaskerx.wbsrisktaskerx.service.export;
 
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PUBLIC)
 public class ExportDetails {
     String password;
     String currentDate;
-
-    ExportDetails(String password, String currentDate) {
-        this.password = password;
-        this.currentDate = currentDate;
-    }
 }
