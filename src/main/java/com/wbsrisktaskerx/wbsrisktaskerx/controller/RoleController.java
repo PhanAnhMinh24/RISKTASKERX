aadea287e25eaa285f702dabafe03014d0542800
xuanngoczxc <xuanngoczxc@gmail.com>
2025-04-09 16:40:32 +0700

    List, search, filter role

--- src/main/java/com/wbsrisktaskerx/wbsrisktaskerx/controller/RoleController.java
+++ src/main/java/com/wbsrisktaskerx/wbsrisktaskerx/controller/RoleController.java
@@ -2,11 +2,15 @@ package com.wbsrisktaskerx.wbsrisktaskerx.controller;
 
 import com.wbsrisktaskerx.wbsrisktaskerx.common.constants.EndpointConstants;
 import com.wbsrisktaskerx.wbsrisktaskerx.pojo.ApiResult;
+import com.wbsrisktaskerx.wbsrisktaskerx.pojo.PagingRequest;
 import com.wbsrisktaskerx.wbsrisktaskerx.pojo.request.ActiveRoleRequest;
 import com.wbsrisktaskerx.wbsrisktaskerx.pojo.request.RoleRequest;
+import com.wbsrisktaskerx.wbsrisktaskerx.pojo.request.SearchFilterRoleRequest;
+import com.wbsrisktaskerx.wbsrisktaskerx.pojo.response.AdminResponse;
 import com.wbsrisktaskerx.wbsrisktaskerx.pojo.response.RoleResponse;
 import com.wbsrisktaskerx.wbsrisktaskerx.service.role.IRoleService;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -29,5 +33,11 @@ public class RoleController {
     public ResponseEntity<ApiResult<Boolean>> updateRoleActive(@RequestBody ActiveRoleRequest request) {
         boolean result = roleService.updateIsActive(request);
         return ResponseEntity.ok(ApiResult.success(result));
+    }
+
+    @PostMapping(EndpointConstants.SEARCH_FILTER)
+    public ResponseEntity<ApiResult<Page<RoleResponse>>> searchFilterRole(@RequestBody PagingRequest<SearchFilterRoleRequest> request) {
+        Page<RoleResponse> pageResult = roleService.searchAndFilterRole(request);
+        return ResponseEntity.ok(ApiResult.success(pageResult));
     }
 }
