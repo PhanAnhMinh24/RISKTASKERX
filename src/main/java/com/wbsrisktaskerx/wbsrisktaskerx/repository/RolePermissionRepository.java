802ce45dff9379db191ce76c7ff0d414e9c78ab2
xuanngoczxc <xuanngoczxc@gmail.com>
2025-04-21 16:19:26 +0700

    Delete role

--- src/main/java/com/wbsrisktaskerx/wbsrisktaskerx/repository/RolePermissionRepository.java
+++ src/main/java/com/wbsrisktaskerx/wbsrisktaskerx/repository/RolePermissionRepository.java
@@ -1,5 +1,6 @@
 package com.wbsrisktaskerx.wbsrisktaskerx.repository;
 
+import com.wbsrisktaskerx.wbsrisktaskerx.entity.Role;
 import com.wbsrisktaskerx.wbsrisktaskerx.entity.RolePermission;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -9,6 +10,5 @@ import java.util.List;
 @Repository
 public interface RolePermissionRepository extends JpaRepository<RolePermission, Integer> {
     List<RolePermission> findByRoleId(Integer roleId);
-    void deleteByRoleIdAndPermissionId(Integer roleId, Integer permissionId);
-    List<RolePermission> findByRoleIdAndPermissionIdIn(Integer roleId, List<Integer> permissionIds);
+    void deleteByRole(Role role);
 }
